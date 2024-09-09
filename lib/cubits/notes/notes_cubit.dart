d0c46427f261d1fc4113dc9c660870d0348a3936
itiswd <itiswdev97@gmail.com>
2024-09-09 14:48:36 +0300

    Notes cubit done.

--- lib/cubits/notes/notes_cubit.dart
+++ lib/cubits/notes/notes_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_note/core/app_constants.dart';
 import 'package:hive_note/models/note_model.dart';
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  void fetchAllNotes() {
+    try {
+      final notesBox = Hive.box<NoteModel>(kNoteBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes: notes));
+    } catch (e) {
+      emit(NotesFailed(message: e.toString()));
+    }
+  }
 }
--- lib/views/add_note_view.dart
+++ lib/views/add_note_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_note/cubits/notes/notes_cubit.dart';
 import 'package:hive_note/views/widgets/custom_floating_action_button.dart';
 import 'package:hive_note/views/widgets/add_note_view_body.dart';
 
@@ -7,10 +9,13 @@ class AddNoteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      resizeToAvoidBottomInset: false,
-      floatingActionButton: CustomFloatingActionButton(),
-      body: HomeViewBody(),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: const Scaffold(
+        resizeToAvoidBottomInset: false,
+        floatingActionButton: CustomFloatingActionButton(),
+        body: HomeViewBody(),
+      ),
     );
   }
 }
