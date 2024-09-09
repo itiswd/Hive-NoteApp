2cd04fff0e78c8d1819aca3f8b2a38d64d1027e7
itiswd <itiswdev97@gmail.com>
2024-09-09 07:31:46 +0300

    Add Edit Note View and Navigation to Note Item
    Created a new `EditNoteView` and its corresponding `EditNoteViewBody` widget to facilitate note editing. Updated the `NoteItem` widget to include an `InkWell` wrapper that navigates to the `EditNoteView` when tapped. This change enhances the user experience by allowing direct access to the edit functionality from the note list.

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_note/core/app_colors.dart';
 import 'package:hive_note/core/app_constants.dart';
 import 'package:hive_note/core/app_styles.dart';
+import 'package:hive_note/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({
@@ -11,44 +12,55 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(24.0.r),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16.0.r),
-        color: AppColors.green,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            contentPadding: const EdgeInsets.all(0),
-            title: Text(
-              'Flutter Tips',
-              style: AppStyles.title,
-            ),
-            subtitle: Padding(
-              padding:
-                  EdgeInsets.symmetric(vertical: AppConstants.kVerticalPadding),
-              child: Text(
-                'Build your own apps with Flutter, Dart, and more!',
-                style: AppStyles.body,
+    return InkWell(
+      borderRadius: BorderRadius.circular(16.0.r),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const EditNoteView(),
+          ),
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.all(24.0.r),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0.r),
+          color: AppColors.green,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.all(0),
+              title: Text(
+                'Flutter Tips',
+                style: AppStyles.title,
+              ),
+              subtitle: Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: AppConstants.kVerticalPadding),
+                child: Text(
+                  'Build your own apps with Flutter, Dart, and more!',
+                  style: AppStyles.body,
+                ),
+              ),
+              trailing: InkWell(
+                borderRadius: BorderRadius.circular(16.0.r),
+                onTap: () {},
+                child: Image.asset(
+                  width: 28.0.h,
+                  height: 28.0.h,
+                  'assets/images/delete.png',
+                ),
               ),
             ),
-            trailing: InkWell(
-              borderRadius: BorderRadius.circular(16.0.r),
-              onTap: () {},
-              child: Image.asset(
-                width: 28.0.h,
-                height: 28.0.h,
-                'assets/images/delete.png',
-              ),
+            Text(
+              '23/12/2022',
+              style: AppStyles.date,
             ),
-          ),
-          Text(
-            '23/12/2022',
-            style: AppStyles.date,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
