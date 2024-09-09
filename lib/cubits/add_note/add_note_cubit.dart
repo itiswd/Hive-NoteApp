582f3f75e19317f9df3fdef29ff5bdc945d49242
itiswd <itiswdev97@gmail.com>
2024-09-09 14:42:09 +0300

    Add notes cubit and its states.

--- lib/cubits/add_note/add_note_cubit.dart
+++ lib/cubits/add_note/add_note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_note/core/app_constants.dart';
---
+++ lib/cubits/notes/notes_state.dart
@@ -0,0 +1,22 @@
+part of 'notes_cubit.dart';
+
+@immutable
+sealed class NotesState {}
+
+final class NotesInitial extends NotesState {}
+
+final class NotesLoading extends NotesState {}
+
+final class NotesSuccess extends NotesState {
+  final List<NoteModel> notes;
+  NotesSuccess({
+    required this.notes,
+  });
+}
+
+final class NotesFailed extends NotesState {
+  final String message;
+  NotesFailed({
+    required this.message,
+  });
+}
