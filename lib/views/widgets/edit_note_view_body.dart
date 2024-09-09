b4ed9fa898adb738203e30a4cc7ae0e915bbdb43
itiswd <itiswdev97@gmail.com>
2024-09-09 09:35:20 +0300

    Edit note ui done

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_note/core/app_constants.dart';
 import 'package:hive_note/views/widgets/custom_app_bar.dart';
+import 'package:hive_note/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -12,12 +14,16 @@ class EditNoteViewBody extends StatelessWidget {
         padding: EdgeInsets.symmetric(
           horizontal: AppConstants.kHorizontalPadding,
         ),
-        child: const Column(
+        child: Column(
           children: [
-            CustomAppBar(
+            const CustomAppBar(
               text: 'Edit Note',
               icon: Icons.done,
             ),
+            SizedBox(height: 16.0.w),
+            const CustomTextField(title: 'Title'),
+            SizedBox(height: 8.0.w),
+            const CustomTextField(title: 'Content', maxLines: 5),
           ],
         ),
       ),
